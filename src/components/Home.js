8a3f88a1f585a99f7f85e742363743a2a1889c82
Piyush-Rathor1 <piyush.rathor@cardse.co>
2022-08-27 13:36:00 +0530

    remove favicon.ico

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,31 +24,69 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const [loadding, setLoadding] = useState(true);
   useEffect(() => {
     (async () => {
+      setLoadding(true);
       const { data } = await api.getPosts();
       setData(data);
     })();
+    setLoadding(false);
   }, []);
 
   return (
     <Stack alignItems="center" sx={{ minHeight: "100vh", width: "100vw" }}>
-      <Box sx={{ height: 143, background: "#E9E9E9", margin: 0, width: "100%", display: "flex", alignItems: "center", padding: "4em", marginBottom: "2em",}}>
+      <Box
+        sx={{
+          height: 143,
+          background: "#E9E9E9",
+          margin: 0,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          padding: "4em",
+          marginBottom: "2em",
+        }}
+      >
         <Typography variant="h5">Eqaim Blog</Typography>
       </Box>
-      <Paper sx={{minHeight: "600px",width: "100vw",maxWidth: "1200px",height: "100%",position: "relative",}} justifyContent="center" alignItems="center">
-        <Grid container spacing={{ xs: 2, sm: 4, md: 12 }} p={{ xs: 2, sm: 3, md: 5 }}>
-          {data.length>0&&data.map((d, index) => (
-            <Grid item xs={6} sm={4} md={4} key={index}>
-              <Link to={`/post/${d._id}`} style={{ textDecoration: "none" }}>
-                <Item>{d.title}</Item>
-              </Link>
-            </Grid>
-          ))}
+      <Paper
+        sx={{
+          minHeight: "600px",
+          width: "100vw",
+          maxWidth: "1200px",
+          height: "100%",
+          position: "relative",
+        }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid
+          container
+          spacing={{ xs: 2, sm: 4, md: 12 }}
+          p={{ xs: 2, sm: 3, md: 5 }}
+        >
+          {data.length > 0 &&
+            data.map((d, index) => (
+              <Grid item xs={6} sm={4} md={4} key={index}>
+                <Link to={`/post/${d._id}`} style={{ textDecoration: "none" }}>
+                  <Item>{d.title}</Item>
+                </Link>
+              </Grid>
+            ))}
         </Grid>
         <Box sx={{ position: "absolute", bottom: 24, right: 55 }}>
           <Link to="/add">
-            <Stack sx={{background: "#E9E9E9",height: 70,width: 80,borderRadius: 5, }} alignItems="center" justifyContent="center">
+            <Stack
+              sx={{
+                background: "#E9E9E9",
+                height: 70,
+                width: 80,
+                borderRadius: 5,
+              }}
+              alignItems="center"
+              justifyContent="center"
+            >
               <img src={icon} alt="add" />
             </Stack>
           </Link>
